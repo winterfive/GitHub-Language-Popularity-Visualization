72044b4a865fe19ea7f19f8f98e75b330a95d6aa
Lee <winterfive@users.noreply.github.com>
2017-07-05 16:20:17 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
 
 from flask import render_template
+from cs50 import SQL
 import requests
 import csv
-import os.path
-import pathlib
+# import os.path
+# import pathlib
+
+# create variable for sqlite table connection
+db = SQL("sqlite:///data.db")
 
 def lookup(n):
     """Run API call for language selected"""
     
     # query GitHub for info
     # Make an api call and store the response
-    url = 'https://api.github.com/search/repositories?q=language:{}&sort=stars'.format(n)
+    url = "https://api.github.com/search/repositories?q=language:{}&sort=stars".format(n)
     r = requests.get(url)
     
     # check r
     if r.status_code != 200:
-        raise RuntimeError('API Call status not OK')
+        raise RuntimeError("API Call status not OK")
         return 3
         
     r = r.json()
@@ -27,33 +31,36 @@ def lookup(n):
     except:
         return 2
         
-    if r['incomplete_results'] != False:
-        raise RuntimeError("'Incomplete Results' is True")
+    if r["incomplete_results"] != False:
+        raise RuntimeError("Incomplete Results is True")
         return 4
         
     # Create stars sum variable
     total_stars = 0
     
     # Create and store total # of stars in variable, total_stars
-    repo_dicts = r['items']
+    repo_dicts = r["items"]
     for repo_dict in repo_dicts:
-        total_stars += int(repo_dict['stargazers_count'])
+        total_stars += int(repo_dict["stargazers_count"])
         
     # get average stars per repository
     total_stars = round(total_stars / 30)
-        
-    # write data to csv file
-    ofile = open('data.csv', "a")
-    writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
     
-    # write specific info into csv
-    writer.writerow([n , total_stars])
-     
-    # ifile.close()
-    ofile.close()
-        
-    # return total # of stars
-    # return total_stars
+     # check if table exists already, if not, create
+    db.execute("CREATE TABLE IF NOT EXISTS githubData ('langNames' CHAR(20) UNIQUE, 'stars' INTEGER)")
+    
+    # place new data in sqlite table
+    db.execute("INSERT OR REPLACE INTO githubData (langNames, stars) VALUES(:langNames, :stars)",\
+        langNames = n,\
+        stars = total_stars)
+    
+    # get csv from sqlite table to pass to d3 script
+    langInfos = db.execute("SELECT * FROM githubData")
+    with open("data.csv", "w") as f:
+        writer = csv.writer(f)
+        # writer.writerow(["langNames", "stars"])
+        writer.writerow(langInfos)
+    
     return
     
 def nope(top="", bottom=""):
@@ -71,7 +78,14 @@ def nope(top="", bottom=""):
     return render_template("nope.html", top=escape(top), bottom=escape(bottom))
     
 def erase_csv():
-    """Erases all data from csv file"""
-    f = open("data.csv", "w")
+    """Erases all data from sqlite table"""
+    """Deletes csv file"""
+    
+    # delete rows from table
+    db.execute("DELETE FROM githubData")
+    
+    # delete data from csv file
+    f = open("data.csv", "r+")
     f.truncate()
     f.close()
+    
