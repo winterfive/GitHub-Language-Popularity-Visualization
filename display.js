66dfefe4adf996186b9988dabc7f4574ab21d120
Lee <winterfive@users.noreply.github.com>
2017-07-10 17:06:15 -0400

    Update display.js

--- display.js
+++ display.js
@@ -13,31 +13,31 @@ d3.csv("static/data.csv", function(dataset) {
     svg.selectAll("rect")
     .data(dataset)
     .enter()
-        .append("rect")
-        .attr("x", function(d, i) {
-            return i * (w / dataset.length); 
-        })
-        .attr("y",  function(d) {
-            return h - d.stars;
-        })
-        .attr("width", w / dataset.length - barPadding)
-        .attr("height", function(d) {
-            return d.stars;
-        })
-        .attr("fill", "#999");
+    .append("rect")
+    .attr("x", function(d, i) {
+        return i * (w / dataset.length); 
+    })
+    .attr("y",  function(d) {
+        return h - d.stars;
+    })
+    .attr("width", w / dataset.length - barPadding)
+    .attr("height", function(d) {
+        return d.stars;
+    })
+    .attr("fill", "#e9e2da");
         
-    canvas.selectAll("text")
+    svg.selectAll("text")
         .data(dataset)
         .enter()
-            .append("text")
-            .attr("fill", "white")
-            .attr("y",  function(d) {
-                return h - d.stars + 5;
-            })
-            .text(function (d) {
-                return d.langName;
-            });
-});  
-
-    
-    
+        .append("text")
+        .attr("fill", "black")
+        .text(function (d) {
+            return d.langName;
+        })
+        .attr("transform", function(d,i) {
+        var xText = i * (w / dataset.length) + 20;
+        // var yText = h - yScale(d.v);
+        var yText = h - 15;
+        return "translate(" + xText + "," + yText + "), rotate(-90)";
+      });
+});
